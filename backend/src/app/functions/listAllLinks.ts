c1a0bf68aca03a922ea43b18c75d94e468a6e1d4
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-05-05 19:37:55 -0300

    fix: sort by id (serial)

--- backend/src/app/functions/listAllLinks.ts
+++ backend/src/app/functions/listAllLinks.ts
@@ -4,11 +4,12 @@ import { linksTable } from "@/infra/db/schemas/links";
 import { makeSuccess } from "@/infra/shared/either";
 import type { InferSelectModel } from "drizzle-orm";
 
-type Link = Pick<InferSelectModel<typeof linksTable>, "originalURL" | "shortenedURL" | "numberOfAccesses">;
+type Link = Pick<InferSelectModel<typeof linksTable>, "id" | "originalURL" | "shortenedURL" | "numberOfAccesses">;
 
 export async function listAllLinks() {
    const links: Link[] = await db
       .select({
+         id: linksTable.id,
          originalURL: linksTable.originalURL,
          shortenedURL: linksTable.shortenedURL,
          numberOfAccesses: linksTable.numberOfAccesses
--- frontend/src/components/LinkList.tsx
+++ frontend/src/components/LinkList.tsx
@@ -3,6 +3,7 @@ import { LinkCard } from "./LinkCard";
 import { DownloadSimpleIcon } from "@phosphor-icons/react";
 
 type Link = {
+   id: number,
    originalURL: string;
    shortenedURL: string;
    numberOfAccesses: number
@@ -22,7 +23,7 @@ export function LinkList({ onDownload }: Props) {
    function fetchLinks() {
       fetch("http://localhost:3333/shortlinks")
          .then(r => r.json())
-         .then(setLinks);
+         .then((data: Link[]) => setLinks([...data].sort((a, b) => b.id - a.id)));
    }
 
    function handleVisibility() {
