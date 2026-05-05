98dca5ca7752e59e98b765cd2dcd28783f7250ca
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-05-05 19:08:48 -0300

    fix: style redirect page

--- frontend/src/components/RedirectPage.tsx
+++ frontend/src/components/RedirectPage.tsx
@@ -9,11 +9,19 @@ export function RedirectPage() {
    }, [slug]);
 
    return (
-      <div className="flex flex-col items-center justify-center min-h-screen gap-4">
-         <p className="text-gray-500">Redirecting...</p>
-            <a href={`http://localhost:3333/${slug}`} className="text-blue-600 underline">
-               Not redirected? Click here.
-            </a>
+      <div className="flex flex-col items-center justify-center min-h-screen gap-3 bg-gray-100">
+         <div className="flex items-center gap-2 text-blue-base font-bold text-lg mb-4">
+            <span className="size-7 rounded-full bg-blue-base inline-block" />
+            brev.ly
+         </div>
+         <p className="text-md text-gray-500">Redirecionando...</p>
+         <a
+            href={`http://localhost:3333/${slug}`}
+            className="text-sm text-blue-base underline hover:text-blue-dark"
+         >
+            Não redirecionou? Clique aqui.
+         </a>
       </div>
    );
 }
+
